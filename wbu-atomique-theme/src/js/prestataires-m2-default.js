be29f3a552880aeec221e9a19c28292fee95695b
stephane888 <kksasteph888@gmail.com>
2021-10-13 17:33:04 +0200

    MAJ

--- wbu-atomique-theme/src/js/prestataires-m2-default.js
+++ wbu-atomique-theme/src/js/prestataires-m2-default.js
@@ -1,1 +1,5 @@
+// theme-bigger
+import "@stephane888/wbu-atomique/js/swiper/swiper-big.js";
+import "@stephane888/wbu-atomique/js/swiper/swiper-unique.js";
+import "@stephane888/wbu-atomique/js/swiper/swiper.js";
 import "../scss/m2/prestataires-m2-default.scss";
--- wbu-atomique-theme/src/js/prestataires-m3-default.js
+++ wbu-atomique-theme/src/js/prestataires-m3-default.js
@@ -1,3 +1,4 @@
+// theme-extra
 import "@stephane888/wbu-atomique/js/swiper/swiper-big-v2.js";
 import "@stephane888/wbu-atomique/js/swiper/swiper-big-v3.js";
 import "../scss/m3/prestataires-m3-default.scss";
