b8827becf5124764f9982de1a6441f87742236db
stephane888 <kksasteph888@gmail.com>
2021-10-13 21:21:45 +0200

    Hauteur min pour le logo

--- wbu-atomique-theme/src/js/prestataires-m3-default.js
+++ wbu-atomique-theme/src/js/prestataires-m3-default.js
@@ -1,4 +1,5 @@
 // theme-extra
 import "@stephane888/wbu-atomique/js/swiper/swiper-big-v2.js";
 import "@stephane888/wbu-atomique/js/swiper/swiper-big-v3.js";
+import "@stephane888/wbu-atomique/js/swiper/swiper-unique.js";
 import "../scss/m3/prestataires-m3-default.scss";
