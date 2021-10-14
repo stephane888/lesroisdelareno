50ae95273ece0f021603d18de4ed8beab8889c83
stephane888 <kksasteph888@gmail.com>
2021-10-14 08:35:58 +0200

    ajout des styles sur le theme gabi

--- wbu-atomique-theme/src/js/prestataires-m1-default.js
+++ wbu-atomique-theme/src/js/prestataires-m1-default.js
@@ -1,2 +1,4 @@
 import "../scss/m1/prestataires-m1-default.scss";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "@stephane888/wbu-atomique/js/swiper/swiper.js";
+import "@stephane888/wbu-atomique/js/swiper/swiper-unique.js";
